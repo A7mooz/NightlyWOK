fff2f8373347db2853591021ec7993701e21cdc1
A7mooz <teaming04@gmail.com>
2023-03-03 21:13:59 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NightlyWok
+
+A handler to fix type issues with WoKCommands and provide a faster solutions.
---
+++ tsup.config.ts
@@ -0,0 +1,42 @@
+import { Options } from 'tsup';
+import { defineConfig } from 'tsup';
+const shared = {
+	entry: ['src/index.ts'],
+	platform: 'node',
+	tsconfig: 'tsconfig.base.json',
+	external: ['discord.js'],
+	clean: true,
+	minify: true,
+} satisfies Options;
+
+export default defineConfig([
+	{
+		format: 'esm',
+		target: 'node16',
+		outDir: './dist/esm',
+		treeshake: true,
+		outExtension() {
+			return {
+				js: '.mjs',
+			};
+		},
+		...shared,
+	},
+	{
+		format: 'cjs',
+		splitting: false,
+		target: 'node16',
+		outDir: './dist/cjs',
+		outExtension() {
+			return {
+				js: '.cjs',
+			};
+		},
+		...shared,
+	},
+	{
+		outDir: './dist/types',
+		dts: { only: true },
+		...shared,
+	},
+]);
